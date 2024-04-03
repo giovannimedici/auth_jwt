0c284513b693861417bf869947e05ac3a12719a7
giovannimedici <giovannigilsu@gmail.com>
2024-04-02 22:19:43 -0300

    first commit

--- app.js
+++ app.js
@@ -18,6 +18,19 @@ app.get('/', (req, res) => {
     res.status(200).json({ msg: 'Bem vindo a nossa API!' })
 })
 
+// Private Route
+app.get('/user/:id', async (req, res) => {
+    const id = req.params.id
+
+    //check if user exists and hides the password
+    const user = await User.findById(id, "-password");
+
+    if (!user) {
+        return res.status(404).json({ msg: "Usuário não encontrado!" });
+    }
+    res.status(200).json({ user })
+})
+
 // Register User
 app.post('/auth/register', async (req, res) => {
     const { name, email, password, confirmPassword } = req.body
@@ -58,8 +71,56 @@ app.post('/auth/register', async (req, res) => {
     try {
         await user.save()
 
-        res.status(201).json({msg: 'Usuário criado com sucesso'})
+        res.status(201).json({ msg: 'Usuário criado com sucesso' })
 
+    } catch (error) {
+        console.log(error)
+
+        res
+            .status(500)
+            .json({
+                msg: 'Aconteceu um erro no servidor, tente novamente mais tarde!'
+            })
+    }
+})
+
+// Login User 
+app.post('/auth/login', async (req, res) => {
+    const { email, password } = req.body
+
+    if (!email) {
+        return res.status(422).json({ msg: 'O email é obrigatório!' })
+    }
+
+    if (!password) {
+        return res.status(422).json({ msg: 'A senha é obrigatória!' })
+    }
+
+    // check if user exists
+    const user = await User.findOne({ email: email })
+
+    if (!user) {
+        return res.status(404).json({ msg: 'Usuário não encontrado.' })
+    }
+
+    //check if password match
+    const checkPassword = await bcrypt.compare(password, user.password)
+
+    if (!checkPassword) {
+        return res.status(422).json({ msg: 'Senha inválida' })
+    }
+
+    try {
+        const secret = process.env.SECRET
+
+        const token = jwt.sign(
+            {
+                id: user._id,
+            },
+            secret,
+        )
+
+        res.status(200).json({ msg: 'Autenticação realizada com sucesso', token })
     } catch (error) {
         console.log(error)
 
