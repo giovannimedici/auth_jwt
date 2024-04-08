b2e08df6087e7df48935f4fffdb4cbaa58813a75
giovannimedici <giovannigilsu@gmail.com>
2024-04-08 19:42:42 -0300

    API Finished

--- app.js
+++ app.js
@@ -19,17 +19,43 @@ app.get('/', (req, res) => {
 })
 
 // Private Route
-app.get('/user/:id', async (req, res) => {
+app.get('/user/:id', checkToken, async (req, res) => {
     const id = req.params.id
 
     //check if user exists and hides the password
-    const user = await User.findById(id, "-password");
+    try {
+        const user = await User.findById(id, "-password");
 
-    if (!user) {
-        return res.status(404).json({ msg: "Usuário não encontrado!" });
+        if (!user) {
+            return res.status(404).json({ msg: "Usuário não encontrado!" });
+        }
+        res.status(200).json({ user })
     }
-    res.status(200).json({ user })
+    catch (err) {
+        console.log(err);
+    }  
 })
+
+function checkToken(req, res, next){
+    const authHeader = req.headers['authorization']
+    const token  = authHeader && authHeader.split(' ')[1]
+
+    if(!token){
+        return res.status(401).json({msg: 'Acesso negado!'})
+    }
+
+    try{
+        const secret = process.env.SECRET
+
+        jwt.verify(token, secret)
+
+        next()
+    } catch(error){
+        res.status(400).json({msg: "Token inválido!"})
+    }
+}
+
+
 
 // Register User
 app.post('/auth/register', async (req, res) => {
